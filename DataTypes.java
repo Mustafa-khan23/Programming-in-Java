8368973b5d91318af77a3e7ce2afc01251261435
= <=>
2026-06-22 18:44:06 +0530

    DataTypes in Java

---
+++ DataTypes.java
@@ -0,0 +1,11 @@
+public class DataTypes {
+    public static void main(String[] args) {
+        int age = 19; //integer Type variable and for big values long is used
+        double PI = 3.14; //Decimal Type value for short values float is used
+        String name = "Mustafa"; //String type is used to store more then 1 character
+        boolean value = true; //boolean is true/false values
+        byte shortValue = 127; //byte stores in range of -128-127 numbers
+        short numbers = 4343; //short type stores value numbers from -32,768-32,767
+        char Alphabet = 'A'; //char stores only one character a-z or A-Z
+    }
+}
