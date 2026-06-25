a6ec2cfe928b435d37a8130beb8191905c2b3dec
Mustafa-khan23 <mustafakhan3237@gmail.com>
2026-06-25 23:54:28 +0530

    Structured the Java files

---
+++ Loops/loopsInJava.java
@@ -0,0 +1,25 @@
+public class loopsInJava {
+    public static void main(String args[]) {
+        //for loop :
+        System.out.println("This is by for loop");
+        for(int i = 1; i <= 10; i++) {
+            System.out.println(i);
+        }
+
+        //while loop :
+        System.out.println("This is by while loop");
+        int i = 1;
+        while (i <= 10) {
+            System.out.println(i);
+            i++;
+        }
+
+        //do-while loop : 
+        System.out.println("This is by do-while loop");
+        int j = 1;
+        do {
+            System.out.println(j);
+            j++;
+        } while(j <= 10);
+    }
+}
